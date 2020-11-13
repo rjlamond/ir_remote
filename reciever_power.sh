ddd42df556f50bbccb0a34bb1ecff040c5813934
Rory Lamond <pi@raspberrypizero>
2020-11-13 15:32:04 -0500

    Initial commit

---
+++ reciever_power.sh
@@ -0,0 +1,3 @@
+irsend SEND_START pioneer KEY_POWER
+sleep .5
+irsend SEND_STOP pioneer KEY_POWER
---
+++ toggle_media.sh
@@ -0,0 +1,7 @@
+irsend SEND_START sharp KEY_POWER
+sleep .5
+irsend SEND_STOP sharp KEY_POWER
+sleep.1
+irsend SEND_START pioneer KEY_POWER
+sleep .5
+irsend SEND_STOP pioneer KEY_POWER
